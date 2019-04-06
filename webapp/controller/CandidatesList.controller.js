f4d04d79da38e3d27d6d063725fd031696c103b3
prudhvi1228 <sriram.dandu1228@gmail.com>
2019-04-06 06:31:58 +0000

    generated JS Doc

--- webapp/controller/CandidatesList.controller.js
+++ webapp/controller/CandidatesList.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
+
 ], function (Controller) {
 	"use strict";
 
@@ -8,15 +9,99 @@ sap.ui.define([
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * Called to get the local Storage value as Model when refreshed the data can be retrived again.
+		 * Checks for the local storage if local storage empty or not. If it is not empty then setting local storage data to globalData JOSN model.
 		 * @memberOf com.cassinitech.CandidateTracker.CandidateTracker.view.CandidatesList
 		 */
-		onInit: function () {
 
+		onInit: function () {
+			var oLocalStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+			var data = oLocalStore.get("localData");
+			if (data !== null) {
+				var oModel = this.getOwnerComponent().getModel("globalData");
+				oModel.setData(data);
+			}
+
+		},
+		
+		
+		/** 
+		 * Pops up the CandidatePopover Fragment with selected candidates data filed in.
+		 * Initialize and open the POPOVER. 
+		 * Getting the Selected candidates data as Object and Path, Setting the Object to JSON Modle to selectedDataModel(Model Name).
+		 * @param oEvent returns the ITEM List of the Table
+		 */
+		onPress: function (oEvent) {
+			if (!this._oPopover) {
+				this._oPopover = sap.ui.xmlfragment("com.cassinitech.CandidateTracker.CandidateTracker.view.fragments.CandidatePopover", this);
+				this.getView().addDependent(this._oPopover);
+
+			}
+			this._oPopover.openBy(oEvent.getSource());
+			var obj = this.getView().byId("table").getBinding("items").oList;
+			var path = oEvent.getSource().getBindingContext("globalData").sPath;
+			path = path.split("/")[2];
+			var jsonModel = new sap.ui.model.json.JSONModel();
+			jsonModel.setData(obj[path]);
+			this.getOwnerComponent().setModel(jsonModel, "selectedDataModel");
+		},
+		/** 
+		 * Closes the CandidatePopover Fragment.
+		 */
+		onClosePopover: function () {
+			this._oPopover.close();
 		},
 
 		/** 
-		 * 
-		 * @param oEvent
+		 * Saves the Edited Candidate data to the globalData and Local Storage on clicking the Update Button.
+		 * Getting the Selected Candidates data as the Object.
+		 * Getting all POPOVER edit form fields by ID.
+		 * Getting the global Model's data (globalData) by Property and looping the Models Data and checking whether the
+		 * selected candidtes id is equal to Model data ID then we assign the edited data to relevant Model (globalData).
+		 * Getting the Model and Setting the Property with the Updated Data(modelData).
+		 * Seeting the related changes to the local Storage and Closes the POPOVER.
+		 */
+		onEditChanges: function () {
+			var oSelectedCandidate = this.getView().getModel("selectedDataModel").getData();
+			var oId = sap.ui.getCore().byId("edtID").getValue();
+			var oName = sap.ui.getCore().byId("edtName").getValue();
+			var oSurname = sap.ui.getCore().byId("edtsurname").getValue();
+			var oSkill = sap.ui.getCore().byId("edtSkill").getValue();
+			var oDate = sap.ui.getCore().byId("edtDate").getValue();
+			var oStatus = sap.ui.getCore().byId("edtStatus").getValue();
+			var modelData = this.getView().getModel("ccc").getProperty("/candidateData");
+
+			for (var i = 0; i < modelData.length; i++) {
+				if (modelData[i].ID === oSelectedCandidate.ID) {
+					modelData[i].ID = oId;
+					modelData[i].Name = oName;
+					modelData[i].Surname = oSurname;
+					modelData[i].Skill = oSkill;
+					modelData[i].InterviewDate = oDate;
+					modelData[i].Status = oStatus;
+				}
+			}
+			this.getView().getModel("globalData").setProperty("/candidateData", modelData);
+			var oLocalStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+			oLocalStore.put("localData", {
+				candidateData: modelData
+			});
+			this._oPopover.close();
+		},
+		/** 
+		 * It logs out and navigates to the Login screen
+		 */
+		onLogout: function () {
+			this.getOwnerComponent().getRouter().navTo("LoginView");
+		},
+
+		/** 
+		 * Serches the data from the Table
+		 * Create an array.
+		 * Check search value if it is not empty,Create Fileter with contains condition and passing the search value.
+		 * Push the filter in the Created Array.
+		 * Getting the binding context of the table and Passing the filter to it.
+		 * @param oEvent returns the parameter value.
 		 */
 		onSearch: function (oEvent) {
 			var query = oEvent.getParameters().newValue;
@@ -30,6 +115,9 @@ sap.ui.define([
 
 		},
 
+		/** 
+		 * Initialize and opening the AddNewCandidate Fragment.
+		 */
 		onAddCandidate: function () {
 			if (!this.AddNewCandidateDialogue) {
 				this.AddNewCandidateDialogue = sap.ui.xmlfragment(
@@ -40,9 +128,16 @@ sap.ui.define([
 			this.AddNewCandidateDialogue.open();
 
 		},
+		/** 
+		 * Closing the AddNewCandidate Fragment.
+		 */
 		onCloseCandidate: function () {
 			this.AddNewCandidateDialogue.close();
 		},
+		/** 
+		 * Saving the data from the AddNewCandidate fragment to the globalData Model and Local Storage when clicked on Add a Candidate Button.
+		 * After saving setting the input value of related fragment as null and close the fragment.
+		 */
 		onSaveCandidate: function () {
 			var id = sap.ui.getCore().byId("idCndId").getValue();
 			var name = sap.ui.getCore().byId("idCndName").getValue();
@@ -56,7 +151,7 @@ sap.ui.define([
 				"Name": name,
 				"Surname": surname,
 				"Skill": skill,
-				"Interview Date": interviewDate,
+				"InterviewDate": interviewDate,
 				"Status": Status
 			};
 
@@ -64,11 +159,10 @@ sap.ui.define([
 			var oModelData = oModel.getProperty("/candidateData");
 			oModelData.push(newObject);
 			oModel.setProperty("/candidateData", oModelData);
-			// var changedData = oModel.getProperty("/candidateData");
-			// var globalJsonModel = new sap.ui.model.json.JSONModel();
-
-			// globalJsonModel.setData(changedData, "globalData");
-			// this.setModel(globalJsonModel, "globalData");
+			var oLocalStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+			oLocalStore.put("localData", {
+				candidateData: oModelData
+			});
 
 			sap.ui.getCore().byId("idCndId").setValue("");
 			sap.ui.getCore().byId("idCndName").setValue("");
@@ -79,6 +173,11 @@ sap.ui.define([
 			this.AddNewCandidateDialogue.close();
 
 		},
+		
+		
+		/** 
+		 * Setting the Pie Chats VizProperties and Title and setting the pieModel.
+		 */
 		onAfterRendering: function () {
 			var oChart = this.getView().byId("pieid");
 			oChart.setVizProperties({
@@ -93,21 +192,6 @@ sap.ui.define([
 			});
 			var json = new sap.ui.model.json.JSONModel("model/data.json");
 			this.getOwnerComponent().setModel(json, "pieModel");
-		},
-
-		onPieChart: function () {
-			if (!this.pieChart) {
-				this.pieChart = sap.ui.xmlfragment(
-					"com.cassinitech.CandidateTracker.CandidateTracker.view.fragments.PieChart", this);
-				this.pieChart.addStyleClass("sapUiSizeCompact");
-				this.getView().addDependent(this.pieChart);
-			}
-
-			this.pieChart.open();
-
-		},
-		onChartClose: function () {
-			this.pieChart.close();
 		}
 
 		/**
